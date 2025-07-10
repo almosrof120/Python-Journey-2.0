ce9c95c5df28ed5d454892007d4606e28672cfb9
almosrof120 <mosrof35-645@diu.edu.bd>
2025-07-11 01:12:45 +0600

    Here is my journey again start after the bachelor journey!

--- intro.py
+++ intro.py
@@ -2,4 +2,4 @@ import numpy as np
 import pandas as pd
 #print("NumPy version:", np.__version__)
 #print("Pandas version:", pd.__version__)
-print("Al Mosrof is ready to use!")
+print("Al Mosrof is ready to use, Python!")
